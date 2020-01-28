c4bd695d7d148f24c84650a6807b3c4bcef04e91
Evan Flack <evanjflack@gmail.com>
2020-01-28 15:19:00 -0500

    added print to unpack_opt

--- R/unpack_opt.R
+++ R/unpack_opt.R
@@ -11,7 +11,21 @@ unpack_opt <- function(option_list) {
     years <- list(years = seq(first_year, last_year))
     invisible(list2env(years, .GlobalEnv))
   }
+  # Print Options
+  if (print == T) {
+    if ("pct" %in% names(opt)) {
+      message("pct = ", opt$pct)
+    }
+    if ("first_year" %in% names(opt) & "last_year" %in% names(opt)) {
+      message("years = ", first_year, "-", last_year)
+    }
+    if ("resp_var" %in% names(opt)) {
+      message("resp_var = ", resp_var)
+    }
+  }
 }
+
+opt["pct"]$pct
 
 #' Print sample options
 #' 
