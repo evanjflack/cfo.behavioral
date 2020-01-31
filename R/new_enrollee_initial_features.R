29cf82cea2385dd029f174d70eb71d20c54b38a1
Evan Flack <evanjflack@gmail.com>
2020-01-31 09:54:34 -0500

    new functions for initial part d new enrollee features

---
+++ R/new_enrollee_initial_features.R
@@ -0,0 +1,45 @@
+#' Create Initial ATC Indicators (New Enrollees)
+#' 
+#' @param DT data.table
+#' @param DT_id data.table
+#' @param atc_ind data.table
+#' @param initial_days integer
+#' 
+#' @return data.table
+indicate_initial_atc <- function(DT, DT_id, atc_ind, initial_days) {
+  initial_DT <- DT %>%
+    .[within_days %between% c(0, initial_days), ] %>%
+    .[, lab_prod := str_pad(lab_prod, 9, pad = "0")] %>% 
+    .[, .(bene_id, lab_prod)]
+  
+  initial_atc <- initial_DT %>% 
+    merge(atc_ind, by = "lab_prod") %>% 
+    .[, lab_prod := NULL] %>% 
+    .[, lapply(.SD, max), by = bene_id] %>% 
+    fill_in_zeros(DT_id[, .(bene_id)], "bene_id")
+  
+  return(initial_atc)
+}
+
+#' Calculate Initial Spending (New Enrollees)
+#' 
+#' @param DT data.table
+#' @param DT_id data.table
+#' @param initial_days integer vector
+#' 
+#' @return data.table
+calc_initial_spending <- function(DT, DT_id, initial_days) {
+  initial_spending <- DT %>% 
+    .[within_days >= 0, ]
+  for (i in initial_days) {
+    message(paste0("initial days = ", i))
+    initial_spending[, paste0("initial_cost_", i) := ifelse(within_days <= i, 
+                                                            cost, 0)]
+  }
+  initial_spending %<>%
+    .[, lapply(.SD, sum), by = .(bene_id),
+      .SDcols = grep("initial_cost_", names(.))] %>% 
+    fill_in_zeros(DT_id[, .(bene_id)], "bene_id")
+  
+  return(initial_spending)
+}
