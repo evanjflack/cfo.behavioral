a9773c70124e1f20936b261cbcd2c9df52725790
Evan Flack <evanjflack@gmail.com>
2020-03-05 08:42:38 -0500

    added documentation for acute outcomes

--- R/acute_event_outcomes.R
+++ R/acute_event_outcomes.R
@@ -1,6 +1,7 @@
-#' Unpack IP Claims
+#' Unpack IP Claims (Acute Outcomes)
 #'
-#' Reformats ip claims diagnosis and procedure codes to long
+#' Reformats ip claims diagnosis and procedure codes from wide to long. Used for
+#' generating acute outcome variables.
 #'
 #' @param ip data.table of inpatient claims with the columns bene_id, clm_id,
 #'  from_dt, and dgnscd1
@@ -14,25 +15,31 @@
 #'
 #' @export
 unpack_ip <- function(ip, num_prcdr) {
+  
+  # Principle diagnosis
   ip_diag <- ip %>%
     .[clm_ln == 1, ] %>%
     .[, c("bene_id", "clm_id", "from_dt", "dgnscd1"), with = FALSE]
 
+  # First "num_prcdr" procedures
   prcdr_vars <- paste0("prcdrcd", seq(1, num_prcdr))
   dt_vars <- paste0("prcdrdt", seq(1, num_prcdr))
 
+  # Reshape procedure codes to long
   ip_prcdrcd <- ip %>%
     .[clm_ln == 1, ] %>%
     .[, c("bene_id", "clm_id", prcdr_vars), with = F] %>%
     melt(id.var = c("bene_id", "clm_id"),
          value.name = "prcdrcd", variable.name = "code_num")
 
+  # Reshape procedure dates to long
   ip_prcdrdt <- ip %>%
     .[clm_ln == 1, ] %>%
     .[, c("bene_id", "clm_id", dt_vars), with = F] %>%
     melt(id.var = c("bene_id", "clm_id"),
          value.name = "prcdrdt", variable.name = "code_num")
-
+  
+  # Combine procedure codes and dates
   ip_prcdr <- cbind(ip_prcdrcd, ip_prcdrdt[, c("prcdrdt"), with = F]) %>%
     .[!(is.na(prcdrcd) | prcdrdt == ""), ]
 
@@ -41,15 +48,23 @@ unpack_ip <- function(ip, num_prcdr) {
 }
 
 #' Create IP Diagnosis Indicators
+#' 
+#' Make indicators for diagnoses in 3 different categories: (1) AMI, (2) stroke, 
+# (3) diabetes complications, and (4) respiratory failure.
 #'
-#' @param DT data.table with ip claims
+#' @param DT data.table with ip diagnoses
 #' @param DT_id data.table with all id_vars
-#' @param id_var character vecotr with names of coumns to aggregate on
+#' @param id_var character vecotr with names of coulumns to aggregate on
 #' @param ami_codes character vector of ami icd9 codes
 #' @param stroke_codes character vector of stroke icd9 codes
 #' @param diab_codes character vector of diabetes icd9 codes
 #'
-#' @return data.table with diagnoisis indicators
+#' @return data.table with id_vars and the following other columns:
+#' \item{ami}{ami indicator}
+#' \item{stroke}{stroke indicator}
+#' \item{diab}{composite diabetes outcome indicator}
+#' \item{rep_fail}{respiratory failure indicator}
+#' \item{resp_arr}{respiratory arrest}
 #'
 #' @export
 create_diag_indicators <- function(DT, DT_id, id_var, ami_codes, stroke_codes,
@@ -62,7 +77,7 @@ create_diag_indicators <- function(DT, DT_id, id_var, ami_codes, stroke_codes,
     .[, stroke := ifelse(substr(dgnscd1, 1, 3) %in% stroke_codes, 1, 0)] %>%
     .[, comp_diab := ifelse(dgnscd1 %in% diab_codes, 1, 0)]
 
-  # Aggregate and fill in 0s
+  # Aggregate by id_vars and fill in 0s
   DT %<>%
     .[, lapply(.SD, max), by = id_var,
       .SDcols = c("ami", "stroke", "resp_fail", "resp_arr", "comp_diab")] %>%
@@ -78,6 +93,8 @@ create_diag_indicators <- function(DT, DT_id, id_var, ami_codes, stroke_codes,
 #' @param id_var character vecotr with names of coumns to aggregate on
 #'
 #' @return data.table with procedure indicators
+#' 
+#' @noRd
 #'
 #' @export
 create_prcdr_indicators <-  function(DT, DT_id, id_var) {
